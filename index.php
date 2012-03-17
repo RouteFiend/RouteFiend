a672eea81688244d95e06b84db7a1061a85ab943
m1 <croxford.miles@gmail.com>
2012-03-17 02:21:23 +0000

    Feature: logout

--- index.php
+++ index.php
@@ -1,7 +1,54 @@
+<?php 
+ 	ini_set('display_errors', 1);
+    error_reporting(E_ALL | E_STRICT);
+
+    session_start();
+    if (isset($_SESSION['user_id'])) {
+    		$url = "in.php";
+			header("location: $url");
+			exit();
+    }
+	if (isset($_POST['submitted'])) {
+		require_once('dbc.php');
+filename
+		$valid = true;
+		if (!(empty($_POST['email']))) {
+			$e = mysql_real_escape_string(trim(strip_tags($_POST['email'])), $dbc);
+			echo $e;
+		}
+		else {
+			$valid = false;
+		}
+		if (!(empty($_POST['password']))) {
+			$p = mysql_real_escape_string(trim(strip_tags($_POST['password'])), $dbc);
+			echo $p;
+		}
+		else {
+			$valid = false;
+		}
+
+		if($valid) {
+			$query = "SELECT user_id, first_name FROM users WHERE email='$e' AND password=SHA('$p')";
+			$result = mysql_query($query);
+			$row = mysql_fetch_array($result, MYSQL_NUM);
+		}
+		if ($row) {
+			//session_start();
+			$_SESSION['user_id'] = $row[0];
+			$_SESSION['name'] = $row[1];
+			$url = "in.php";
+			header("location: $url");
+			exit();
+			}
+		else {
+			echo "nah";
+		}
+	}
+?>
 <html>
 <head>
 	<title>routefiend</title>
-	<link rel="shortcut icon" href="img/rf2.ico">
+	<link rel="shortcut icon" href="img/rf.ico">
 	<style type="text/css">
 	#container{
   width:270px;
@@ -15,13 +62,15 @@
 <body>
 <div id="container"> 
 		<h1>routefiend</h1>
-		<form>
-		<div class="fix"> <input class="input" type="text" placeholder="user" /> </div>
-		<div class="fix"> <input class="input" type="text" placeholder="password" /> </div>
+		<form method="POST" action="index.php">
+		<div class="fix"> <input name="email" class="input" type="text" placeholder="user" /> </div>
+		<div class="fix"> <input name="password" class="input" type="text" placeholder="password" /> </div>
 		<div class="fix">
-			<a href="#" class="btn" id="sign">Sign Up for RouteFiend</a>           
-			<a href="in.php" class="btn" id="log">Login</a>
+			<a href="#" class="btn" id="sign">Sign Up for RouteFiend</a>   
+			<input type="submit">        
+			<!-- <a href="in.php" class="btn" id="log">Login</a> -->
 		</div>
+		<input type="hidden" name="submitted" value="TRUE" />
 		</form>
 	</div>
 </body>
